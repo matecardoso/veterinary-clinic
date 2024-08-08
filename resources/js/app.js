77eeb9e22e2b5b79d714895b0c3f1c0a29d723dd
Mateus Cardoso <dev.mateuscardoso@gmail.com>
2024-08-08 14:52:50 +0000

    add vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,11 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify();
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,7 +19,8 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .use(vuetify)
+            .use(ZiggyVue, Ziggy)
             .mount(el);
     },
     progress: {
